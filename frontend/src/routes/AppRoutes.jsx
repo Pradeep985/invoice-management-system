6fe2adc6395d3668f50e1f07dcb0e9f823767176
Pradeep Yadav <65676546+Pradeep985@users.noreply.github.com>
2026-04-06 15:26:49 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-// Import your page components
-import Dashboard from "../pages/Dashboard"; // Adjust paths based on your folder structure!
+import Dashboard from "../pages/Dashboard"; 
 import Billing from "../pages/Billing";
-
-// Import the new Master pages
 import MasterDashboard from "../pages/MasterDashboard";
 import Customers from "../pages/Customers";
 import Items from "../pages/Items";
@@ -13,11 +9,8 @@ import Items from "../pages/Items";
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* Main Pages */}
       <Route path="/" element={<Dashboard />} />
       <Route path="/billing" element={<Billing />} />
-      
-      {/* Master Data Module */}
       <Route path="/master" element={<MasterDashboard />} />
       <Route path="/master/customers" element={<Customers />} />
       <Route path="/master/items" element={<Items />} />
